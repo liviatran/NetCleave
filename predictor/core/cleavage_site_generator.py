82d240d695768cbed3d3fe23e74f6beca7bef4cd
upcmarina <marina.vallejo01@estudiant.upf.edu>
2022-10-11 11:13:24 +0200

    Generalize function

--- predictor/core/cleavage_site_generator.py
+++ predictor/core/cleavage_site_generator.py
@@ -7,14 +7,15 @@ from Bio import SeqIO
 def readFasta(file):
 
     for seq_record in SeqIO.parse(file, "fasta"):
+        name = seq_record.id
         sequence = seq_record.seq
 
-    return sequence
+    return name,sequence
 
 
 def generateMERS(file,length):
 
-    sequence = readFasta(file)
+    name,sequence = readFasta(file)
 
     peptides_mers = {}
     for index,residue in enumerate(sequence):
@@ -28,53 +29,30 @@ def generateMERS(file,length):
 
 def generateCleavageSites(file):
 
-    protein_sequence = readFasta(file)
+    name,sequence = readFasta(file)
 
-    peptides = []
+    epitopes_id = []
+    epitopes_seq = []
+    epitopes_len = []
     cleavage_sites = []
-    peptides_description = []
 
-    for key in generateMERS(file,8):
-        peptide = generateMERS(file,8)[key]
-        flanking_region = protein_sequence[key+8:key+8+3]
-        cleavage_site = peptide[-4:] + flanking_region
-        if len(cleavage_site)<7:
-            break
-        peptides.append(str(peptide))
-        cleavage_sites.append(str(cleavage_site))
-        peptides_description.append('8mer')
 
-    for key in generateMERS(file,9):
-        peptide = generateMERS(file,9)[key]
-        flanking_region = protein_sequence[key+9:key+9+3]
-        cleavage_site = peptide[-4:] + flanking_region
-        if len(cleavage_site)<7:
-            break
-        peptides.append(str(peptide))
-        cleavage_sites.append(str(cleavage_site))
-        peptides_description.append('9mer')
+    for i in range(8,12):
+        epitope_number = 1
+        for key in generateMERS(file,i):
+            seq = generateMERS(file,i)[key]
+            flanking_region = sequence[key+i:key+i+3]
+            cleavage_site = seq[-4:] + flanking_region
+            if len(cleavage_site)<7:
+                break
+            identifier = str(i)+'mer|'+str(epitope_number)+'|'+seq+'|'+name
+            epitopes_id.append(identifier)
+            epitopes_seq.append(str(seq))
+            cleavage_sites.append(str(cleavage_site))
+            epitopes_len.append(i)
+            epitope_number += 1
 
-    for key in generateMERS(file,10):
-        peptide = generateMERS(file,10)[key]
-        flanking_region = protein_sequence[key+10:key+10+3]
-        cleavage_site = peptide[-4:] + flanking_region
-        if len(cleavage_site)<7:
-            break
-        peptides.append(str(peptide))
-        cleavage_sites.append(str(cleavage_site))
-        peptides_description.append('10mer')
-
-    for key in generateMERS(file,11):
-        peptide = generateMERS(file,11)[key]
-        flanking_region = protein_sequence[key+11:key+11+3]
-        cleavage_site = peptide[-4:] + flanking_region
-        if len(cleavage_site)<7:
-            break
-        peptides.append(str(peptide))
-        cleavage_sites.append(str(cleavage_site))
-        peptides_description.append('11mer')
-
-    df = pd.DataFrame(list(zip(peptides,peptides_description,cleavage_sites)),columns=['epitope','epitope_length','cleavage_site'])
+    df = pd.DataFrame(list(zip(epitopes_id,epitopes_seq,epitopes_len,cleavage_sites)),columns=['epitope_id','epitope','epitope_length','cleavage_site'])
 
     if not os.path.exists('./output/'):
         os.mkdir('./output/')
